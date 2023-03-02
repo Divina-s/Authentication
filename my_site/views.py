eaa3777b1e075f6fd5c6af6d09318e52682d274f
̂̂̂̂jardon237̂ <ngwajude161@gmail.com>
2023-03-02 14:00:15 +0100

    MySite

--- my_site/views.py
+++ my_site/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def register(request, *args, **kwargs):
     return render(request, 'register.html', {})
+def login(request):
+    
+    return render(request, 'login.html', {})
